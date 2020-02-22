4189cabbc2653c174e23ec79211cf60587a4a065
gv4383 <gregory.vargas18@gmail.com>
2020-02-22 16:33:46 -0600

    Added styling to ActionBar component

--- src/components/Input/ActionBar/index.tsx
+++ src/components/Input/ActionBar/index.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react';
 import Button from '../../Input/Button';
 
 const ActionBar: FunctionComponent = () => (
-  <div>
+  <div className="root">
     <Button color="red">Cancel</Button>
     <Button color="red">Save Changes</Button>
   </div>
