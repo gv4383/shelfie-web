cae0b16f4f398b32191fd222df61836946afac0d
gv4383 <gregory.vargas18@gmail.com>
2020-03-08 19:04:35 -0500

    Removed comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { HashRouter } from 'react-router-dom';
 
-// import logo from './logo.svg';
 import Navbar from './components/Header';
 import Routes from './components/Routes';
 
