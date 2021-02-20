a79a3aabe50a79616978c4d95b4040786a15a254
gv4383 <gregory.vargas18@gmail.com>
2021-02-20 15:40:05 -0600

    Add image url attribute to Product type and added type for ApiProduct

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,13 @@
 export interface Product {
   id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+}
+
+export interface ApiProduct {
+  id: number;
+  image_url: string;
   name: string;
   price: number;
 }
